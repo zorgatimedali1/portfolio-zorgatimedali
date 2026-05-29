aac2a2a0b652d8851df01164d946e02df700c982
medalizorgati <medalizorgati8@gmail.com>
2026-05-29 15:20:07 +0100

    Push latest changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
+import imagemin from 'vite-plugin-imagemin'
 
 const plugins = process.env.NODE_ENV === "development"
   ? [inspectAttr(), react()]
-  : [react()];
+  : [react(), imagemin({
+    gifsicle: { optimizationLevel: 3 },
+    optipng: { optimizationLevel: 7 },
+    pngquant: { quality: [0.7, 0.85] },
+    mozjpeg: { quality: 75 },
+    webp: { quality: 75 },
+  })];
 
 export default defineConfig({
   base: '/',
